5ac3cb08a81c30c5872da3d5d14326823c5ebe9e
rayenisme <canriyann@gmail.com>
2026-04-08 16:07:42 +0700

    UserSeeder Commit

---
+++ database/seeders/userSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class userSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+        'name' => 'Admin User',
+        'username' => 'admin',
+        'email' => 'admin@example.com',
+        'password' => bcrypt('admin123')
+    ]);
+    }
+}
